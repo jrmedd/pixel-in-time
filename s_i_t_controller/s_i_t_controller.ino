081adb2d3cf5bbbdeee9892340f8186b74a84ae5
jrmedd <hello@jamesmedd.co.uk>
2019-06-12 19:22:51 +0100

    Add hall effect code to Arduino

--- s_i_t_controller/s_i_t_controller.ino
+++ s_i_t_controller/s_i_t_controller.ino
@@ -1,17 +1,74 @@
 #include "Keyboard.h"
 #include <Bounce2.h>
 
-int p1_magnet = 7;
-Bounce p1_state;
+int passJudge = 7;
+int failJudge = 6;
+
+int newGame = 5;
+
+int p1Magnet = 0;
+
+Bounce passJudgeState;
+Bounce failJudgeState;
+
+Bounce newGameState;
+
+int p1MagnetReading;
+int p1MagnetPrevReading = 0;
+
+int p1TriggerState = 0;
+int p1TriggerPrevState = 0;
+
+int p1TriggerThreshold = 507;
+
+int passCode = 80;
+int failCode = 70;
+int newGameCode = 78;
+int p1Code = 48;
+
 
 void setup() {
-  pinMode(p1_magnet, INPUT_PULLUP);
-  p1_state = Bounce(p1_magnet, 5);
-}
+    //Serial.begin(9600);
+    pinMode(passJudge, INPUT_PULLUP);
+    pinMode(failJudge, INPUT_PULLUP);
+    pinMode(newGame, INPUT_PULLUP);
 
+    passJudgeState = Bounce(passJudge, 5);
+    failJudgeState = Bounce(failJudge, 5);
+    newGameState = Bounce(newGame, 5);
+}
 void loop() {
- p1_state.update();
- if (p1_state.fell()) {
-  Keyboard.write(48);
- }
+    delay(5);
+    p1MagnetReading = analogRead(p1Magnet);
+    //Serial.println(p1MagnetReading);
+    if (p1MagnetReading != p1MagnetPrevReading) {
+        if (p1MagnetReading > p1TriggerThreshold) {
+            p1TriggerState = 1;
+        }
+        else {
+            p1TriggerState = 0;
+        }
+    }
+    if (p1TriggerState != p1TriggerPrevState) {
+        if (p1TriggerState == 1) {
+        Keyboard.write(p1Code);
+        }
+    }
+    p1MagnetPrevReading = p1MagnetReading;
+    p1TriggerPrevState = p1TriggerState;
+
+    passJudgeState.update();
+    failJudgeState.update();
+    newGameState.update();
+
+    if (passJudgeState.fell()) {
+        Keyboard.write(passCode);
+    }
+    if (failJudgeState.fell()) {
+        Keyboard.write(failCode);
+    }
+    if (newGameState.fell()) {
+        Keyboard.write(newGameCode);
+    }
+
 }
